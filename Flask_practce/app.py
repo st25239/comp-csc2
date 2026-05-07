012db243412b56e25c502a01d3cdbb3932441752
Kelllum <st25239@ormiston.school.nz>
2026-05-08 10:45:15 +1200

    phyton code

--- Flask_practce/app.py
+++ Flask_practce/app.py
@@ -1,9 +1,18 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash 
+from flask import Flask, json, render_template, request, redirect, url_for, session, flash 
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
 
-@app.route('/')
+
+@app.route('/index')
 def index():
     return render_template('index.html')    
+
+def load_data():
+    with open('data/flowers.json') as file:
+        flowers = json.load(file)
+        return flowers
+
+if __name__ == '__main__':
+    app.run(debug=True)
