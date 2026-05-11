83b1570576fef6c0a3a7f51b6e00d3a496a53106
Kelllum <st25239@ormiston.school.nz>
2026-05-11 13:23:07 +1200

    stuff and phyton and fixxing stuff

--- Flask_practce/app.py
+++ Flask_practce/app.py
@@ -1,18 +1,29 @@
 from flask import Flask, json, render_template, request, redirect, url_for, session, flash 
-
+import json
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
+
+@app.route('/')
+def index():
+    return render_template('index.html')   
+    return render_template('flowers.json')   
+    return render_template('addons.json')    
+
 
 
 
 @app.route('/index')
 def index():
-    return render_template('index.html')    
+    flowers = load_data()
+    return render_template('index.html', flowers=flowers)
 
 def load_data():
     with open('data/flowers.json') as file:
         flowers = json.load(file)
         return flowers
+    with open('data/addons.json') as file:
+        addons = json.load(file)
+    return ('flowers':  flowers, 'addons'; addons)
 
 if __name__ == '__main__':
     app.run(debug=True)
