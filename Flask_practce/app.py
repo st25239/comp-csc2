9069dc701da3beacc515dacdd6882f951e63e578
Kelllum <st25239@ormiston.school.nz>
2026-05-15 10:14:59 +1200

    fixed code a little bit

--- Flask_practce/app.py
+++ Flask_practce/app.py
@@ -1,32 +1,40 @@
-from flask import Flask, json, render_template, request, redirect, url_for, session, flash 
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 import json
+
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
+
+# Home page
 @app.route('/')
 def index():
-    return render_template('index.html')   
-    return render_template('flowers.json')   
-    return render_template('addons.json')    
+    data = load_data()
+    return render_template(
+        'index.html',
+        flowers=data['flowers'],
+        addons=data['addons']
+    )
 
-@app.route('/Add to cart', methods=['POST'])
+
+# Add to cart page
+@app.route('/add_to_cart', methods=['POST'])
 def add_to_cart():
     return render_template("index1.html")
 
 
-
-@app.route('/index')
-def index():
-    flowers = load_data()
-    return render_template('index.html', flowers=flowers)
-
+# Load JSON data
 def load_data():
     with open('data/flowers.json') as file:
         flowers = json.load(file)
-        return flowers
+
     with open('data/addons.json') as file:
         addons = json.load(file)
-    return {'flowers': flowers, 'addons': addons}
+
+    return {
+        'flowers': flowers,
+        'addons': addons
+    }
+
 
 if __name__ == '__main__':
     app.run(debug=True)
