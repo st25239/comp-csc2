d0822a982b9036bfa9dc0b78bddc7f2bad3502d8
Kelllum <st25239@ormiston.school.nz>
2026-05-20 13:45:57 +1200

    stuff

---
+++ Flask_practce/app1.py
@@ -0,0 +1,39 @@
+import json
+
+from flask import Flask, json, render_template, request, session, flash, redirect, url_for  
+import json
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'
+
+@app.route('/')
+def index():
+    return render_template('index.html')   
+    # return render_template('flowers.json')   
+    # return render_template('addons.json')    
+
+@app.route('/Add to cart', methods=['POST'])
+def add_to_cart():
+    return render_template("index1.html")
+
+@app.route('/index')
+def index():
+    flowers = load_data()
+    return render_template('index.html', flowers=flowers)
+
+def load_data():
+    with open('data/flowers.json') as file:
+        flowers = json.load(file)
+        return flowers
+    with open('data/addons.json') as file:
+        addons = json.load(file)
+    return {'flowers': flowers, 'addons': addons}
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+
+
+
