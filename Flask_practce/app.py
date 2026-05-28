0a10c3f1f20d64b6ab2040415dbe180fdb2d9143
Kelllum <st25239@ormiston.school.nz>
2026-05-28 14:42:57 +1200

    i added stuff and fixed stuff

--- Flask_practce/app.py
+++ Flask_practce/app.py
@@ -3,12 +3,12 @@ import json
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
-@app.route('/')
-def index():
-    return render_template('test.html')   
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/')
+def index(): 
+    flowers, addons = load_data()
+    return render_template('index.html', flowers=flowers, addons=addons)
+
 @app.route('/about')
 def about():
     return render_template('about.html')
@@ -17,9 +17,27 @@ def about():
 def checkout():
     return render_template('checkout.html')
 
+@app.route('/add_to_cart', methods=['POST'])
+def add_to_cart():
+    return render_template('index1.html')
+    flower = request.form.get('flower')
+    quantity = int(request.form.get('quantity'))
+    flowers, addons = load_data()
+    cart = session.get('cart', {})
+
+    
+def load_data():
+    with open('data/flowers.json') as file:
+        flowers = json.load(file)
+    with open('data/addons.json') as file:
+        addons = json.load(file)  
+ 
+    return flowers, addons
 
 
 
 
 
 
+if __name__ == '__main__':
+    app.run(debug=True)
