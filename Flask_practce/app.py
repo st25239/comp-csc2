cbfefa314999e668b6a474ab307f3d3b203bad40
Kelllum <st25239@ormiston.school.nz>
2026-06-03 13:54:13 +1200

    i added session and flowers in cart

--- Flask_practce/app.py
+++ Flask_practce/app.py
@@ -19,13 +19,29 @@ def checkout():
 
 @app.route('/add_to_cart', methods=['POST'])
 def add_to_cart():
-    return render_template('index1.html')
     flower = request.form.get('flower')
     quantity = int(request.form.get('quantity'))
     flowers, addons = load_data()
     cart = session.get('cart', {})
 
+    if flower not in flowers:
+        flash('Invalid flower selection.')
+        return redirect(url_for('index'))
     
+    if flower in cart:
+        cart[flower]['quantity'] += quantity
+    else:
+        cart[flower] = {
+            'quantity': quantity,
+            'price': flowers[flower]['price']
+        }
+
+    session['cart'] = cart #update session
+    session.modified = True # force flask to save the session
+    flash(f'Added {quantity} {flower}(s) added to cart.')
+    return redirect(url_for('index'))
+
+
 def load_data():
     with open('data/flowers.json') as file:
         flowers = json.load(file)
